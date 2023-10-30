8051403477e650079f8d474d4d329daddd23c8f5
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2023-10-30 22:43:39 +0530

    Update main.tf
    adding comment for main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+#This code we are writing to create a demo file using terraform
+
 resource "local_file" "demo_file"{
  filename="/home/ubuntu/terraform-practice/terraform-practice/demo.txt"
  content="Hello this file is created by using Terraform code"
